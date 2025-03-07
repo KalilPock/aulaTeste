bd87945c8752c20198c3dcd370387ca7605687ed
kalilpock <kalilpock54@gmail.com>
2025-03-07 20:54:27 -0300

    atividadeAula

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,31 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
 package com.mycompany.app;
 
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe a quantidade de produtos: ");
+        int quantidadeProdutos = scanner.nextInt();
+
+        double[] precos = new double[quantidadeProdutos];
+        double totalCompra = 0.0;
+
+        for (int i = 0; i < quantidadeProdutos; i++) {
+            System.out.println("Informe o preço do produto " + (i + 1) + ": ");
+            precos[i] = scanner.nextDouble();
+            totalCompra += precos[i]; // Acumula o total da compra
+        }
+
+        System.out.println("Preço total da compra:");
+        for (int i = 0; i < quantidadeProdutos; i++) {
+            System.out.println("Produto " + (i + 1) + ": " + precos[i]);
+        }
+
+        System.out.println("O total da compra é: " + totalCompra); // Exibe o total da compra
+
+        scanner.close();
     }
 }
